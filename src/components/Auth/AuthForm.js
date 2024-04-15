aed3c4ac41d61972321acc6e39103d6deb912bcf
Codetraker <swarajbibhuprasad@gmail.com>
2024-04-15 21:26:47 +0530

    login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,9 +1,41 @@
 import { useRef, useState } from 'react';
 import './AuthForm.css';
 const AuthForm = () =>{
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+    
     const [haveAccount, setHaveAccount] = useState(false);
+
     const submitHandler = (event) =>{
         event.preventDefault();
+        const emailEntered = emailInputRef.current.value;
+        const passwordEntered = passwordInputRef.current.value;
+
+        if(haveAccount){
+
+        }else{
+            fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA8qtTJI1TZiZajaOt0D7WFDkZtrDcoSrM',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: emailEntered,
+                    password: passwordEntered,
+                    returnSecureToken: true
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => {
+            if(res.ok){
+
+            }else{
+                res.json().then(data => {
+                    console.log(data);
+                });
+            }
+        });
+        }
     }
     const loginSignupHandler = () =>{
         setHaveAccount(!haveAccount)
@@ -17,18 +49,17 @@ const AuthForm = () =>{
                         <div className='hello'>{haveAccount ?'Hello,': "Create an account,"}</div>
                         <div className='welcome'>{haveAccount ? 'welcome!':'and Enjoy!'}</div>
 
-                        {/* Add form */}
-                            <div className='loginPassBox'>
+                        <form className='loginPassBox' onSubmit={submitHandler}>
                                 <div className='emailBox'>
-                                    <input type="email" className="email" id="email" placeholder="Enter your Email" required/>
+                                    <input type="email" className="email" id="email" placeholder="Enter your Email" ref={emailInputRef} required/>
                                 </div>
                                 <div className="passwordBox ">
-                                    <input type="password" className="password" id="password" placeholder="Enter Password" required/>
+                                    <input type="password" className="password" id="password" placeholder="Enter Password" ref={passwordInputRef} required/>
                                 </div>
-                            </div>
-                            <div className='loginSigninBox'>
-                                <button className='loginBtn'>{haveAccount ? 'Login' : 'SignUp'}</button>
-                            </div>
+                                <div className='loginSigninBox'>
+                                    <button className='loginBtn'>{haveAccount ? 'Login' : 'SignUp'}</button>
+                                </div>
+                        </form>
 
                         <div className='haveAccount' onClick={loginSignupHandler}>
                             {haveAccount?"Don't have an account? SignUp":"Have an account? LogIn"}
